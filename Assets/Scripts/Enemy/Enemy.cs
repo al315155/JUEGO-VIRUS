ab696b5bd40cd0e4ebcfeac74e9ca201e90f49f9
Rebeca <al315155@uji.es>
2017-10-01 23:43:01 +0200

    Git
    Primer git

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [Tooltip("Initial position where enemy will spawn")]
+    [SerializeField]
+    private Vector3 spawnPosition;
+
+    [Tooltip("Movement speed in any direction")]
+    [SerializeField]
+    private float movementSpeed;
+
+    [Tooltip("Movement speed in any direction")]
+    [SerializeField]
+    private EnemyState enemState;
+
+    void Start () {
+        gameObject.transform.position = spawnPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (enemState)
+        {
+            case EnemyState.ALERT:
+                //TODO State
+                break;
+            case EnemyState.ATTACKING:
+                //TODO State
+                break;
+            case EnemyState.DEAD:
+                //TODO State
+                break;
+            case EnemyState.IDLE:
+                //TODO State
+                break;
+            case EnemyState.MOVING:
+                //TODO State
+                break;
+            case EnemyState.PATROL:
+                //TODO State
+                break;
+            case EnemyState.PURSUIT:
+                //TODO State
+                break;
+        }
+	}
+}
+
+public enum EnemyState
+{
+    IDLE, MOVING, PATROL, PURSUIT, ALERT, ATTACKING, DEAD
+}
---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Movement : MonoBehaviour {
+
+	float x, z;
+	public int lastX = 0, lastZ = 0;
+	public float acceleration = 0f;
+	public float MAX_ACCELERATION;
+	public float velocity;
+
+	void Start () {
+		
+	}
+	
+	void Update () 
+	{
+		
+
+	}
+
+	void FixedUpdate()
+	{
+		//Movimiento básico
+		/*x = Input.GetAxis ("Horizontal");
+		z = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (x + velocity * Time.deltaTime, 0f, z + velocity * Time.deltaTime);
+
+		transform.position += movement;*/
+
+		//Movimiento con efecto de frenazo
+		x = Input.GetAxis("Horizontal");
+		z = Input.GetAxis ("Vertical");
+
+		Vector3 movement;
+		float increase = Time.deltaTime * velocity;
+
+		//Si muevo al personaje
+		if (x != 0 || z != 0) {
+
+			//Detecto la última dirección de movimiento para calcular el impulso final tras el mismo
+			if (x != 0 && z != 0) {
+				if (lastX == 0) lastX = (int)Math.Ceiling (x);
+				if (lastZ == 0) lastZ = (int)Math.Ceiling (z);
+			} else if (x != 0) {
+				if (((int)Math.Ceiling (x) != 0 && lastX != (int)Math.Ceiling (x)) || lastX == 0) lastX = (int)Math.Ceiling (x);
+				lastZ = 0;
+			} else {
+				if (((int)Math.Ceiling (z) != 0 && lastZ != (int)Math.Ceiling (z)) || lastZ == 0) lastZ = (int)Math.Ceiling (z);
+				lastX = 0;
+			}
+
+			//Si ya se está moviendo, y no supera a la aceleración máxima
+			if (acceleration < MAX_ACCELERATION) {
+				//Aumento el valor de la aceleración
+				acceleration += increase;
+			}
+
+			movement = new Vector3 (
+				x * velocity * Time.deltaTime + x * acceleration*acceleration/2, 
+				0f, 
+				z * velocity * Time.deltaTime + z * acceleration*acceleration/2);
+		} 
+		else {
+			//Si no se mueve al jugador, pero tiene aceleración anterior
+			if (acceleration > 0) {
+				//Disminuyo la aceleración
+				acceleration -= increase;
+
+				movement = new Vector3 (
+					lastX * velocity * Time.deltaTime + lastX * acceleration*acceleration/2, 
+					0f, 
+					lastZ * velocity * Time.deltaTime + lastZ * acceleration*acceleration/2);
+			} 
+			//Si ya no existe aceleración
+			else {
+				acceleration = 0f;
+				lastX = 0;
+				lastZ = 0;
+				movement = new Vector3 (0f, 0f, 0f);
+			}
+		}
+
+		//Finalmente, se actualiza la posición del jugador
+		transform.position += movement;
+	}
+
+
+
+}
