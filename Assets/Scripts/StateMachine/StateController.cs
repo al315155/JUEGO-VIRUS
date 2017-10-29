53657f84ddeebe6ee2f7cd612482895ead0d5c71
dharach <al315185@uji.es>
2017-10-29 12:05:04 +0100

    StateMachine
    Añadido Estado Pratrol. Por ahora usa NavMesh para moverse

---
+++ Assets/Scripts/StateMachine/StateController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class StateController : MonoBehaviour {
+
+	public State currentState;
+	public Transform eyes;
+	//public State remainState;
+	public List<Transform> wayPointList;
+
+	[HideInInspector] public NavMeshAgent navMeshAgent;
+	[HideInInspector] public int nextWayPoint;
+
+	void Awake()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+		navMeshAgent.enabled = true;
+	}
+
+	void Update()
+	{
+		currentState.UpdateState (this);
+	}
+
+	void OnDrawGizmos()
+	{
+		if (currentState != null && eyes != null) {
+			Gizmos.color = currentState.sceneGizmoColor;
+			//cambiar radio;
+			Gizmos.DrawWireSphere(eyes.position,1f);
+		}
+	}
+
+
+}
