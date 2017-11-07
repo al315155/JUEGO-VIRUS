27d4855c04ebec8db2d830cccea7304f94ff8f12
María <al188288@uji.es>
2017-11-07 23:21:47 +0100

    Menu principal, game over, jerry ha muerto, y comandos
    falta los creditos

---
+++ Assets/Scripts/Game/JerryHaMuerto.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class JerryHaMuerto : MonoBehaviour {
+
+	// Use this for initialization
+	public void BotonMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void BotonSalir()
+    {
+        Application.Quit();
+    }
+}
