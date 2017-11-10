24293183e0c67322096dae24b51dd90777b6e795
Rebeca <al315155@uji.es>
2017-11-10 04:47:49 +0100

    sdasdas
    assdas1

--- Assets/Scripts/StateMachine/PatrolAction.cs
+++ Assets/Scripts/StateMachine/PatrolAction.cs
@@ -22,6 +22,25 @@ public class PatrolAction : Action {
 		//controller.pathfining.SetTarget (controller.pathfining.wayPoints [1].transform);
 
 		if (controller.pathfining.Finished()) {
+
+			/*if (controller.destination == controller.leftVector) {
+				if (Quaternion.Euler(controller.leftVector) == controller.transform.rotation){
+					controller.AddPass ();
+					controller.destination = controller.rightVector;
+				} else {
+					controller.transform.rotation = Quaternion.RotateTowards(controller.transform.rotation, Quaternion.Euler(controller.leftVector), 3f);
+				}
+			}
+
+			if (controller.destination == controller.rightVector) {
+				if (Quaternion.Euler(controller.rightVector) == controller.transform.rotation){
+					controller.AddPass ();
+					controller.destination = controller.leftVector;
+				} else{
+					controller.transform.rotation = Quaternion.RotateTowards(controller.transform.rotation, Quaternion.Euler(controller.rightVector), 3f);
+				}
+			}*/
+
 			controller.nextWayPoint = (controller.nextWayPoint + 1) % controller.wayPointList.Count;
 			controller.pathfining.SetFinished ();
 		}
